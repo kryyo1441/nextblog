efba31d7a3811cf104f4d84b762e8d538f6ca5af
zelli17 <aayushnair321@gmail.com>
2025-05-30 21:32:41 +0530

    trying to connect prisma client

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
 
-export default function Home() {
+import { prisma } from "./utils/db";
+
+async function getData () {
+  const data = await prisma.BlogPost.findmany({
+    select:{
+      title : true,
+      content: true,
+      createdAt: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      id: true 
+    }
+  })
+
+  return data;
+}
+
+
+export default async function Home() {
+
+  const data = await getData();
   return (
     <div className="py-6 ">
-      <h1 className="text-4xl font-bold tracking-light text-center mb-8">
+      <h1 className="text-3xl font-bold tracking-light mb-6">
         Latest Posts
       </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((data) => (
+          <div key={data.title}>        
+            <h1>{data.title}</h1>
+            <h1>{data.content}</h1>
+          </div>
+      ))}
+      </div>
     </div>
   );
 }
